4995a8da5e30e8fef83cd538fb35991cc8e7918b
YOUR NAME <YOUR@EMAIL.com>
2016-07-20 19:36:16 +0530

    remove migration

--- db/migrate/20160719104008_add_game_to_reviews.rb
+++
@@ -1,5 +0,0 @@
-class AddGameToReviews < ActiveRecord::Migration
-  def change
-    add_reference :reviews, :game, index: true, foreign_key: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -31,10 +31,8 @@ ActiveRecord::Schema.define(version: 20160719152952) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "game_id"
   end
 
-  add_index "reviews", ["game_id"], name: "index_reviews_on_game_id", using: :btree
   add_index "reviews", ["user_id"], name: "index_reviews_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
@@ -57,6 +55,5 @@ ActiveRecord::Schema.define(version: 20160719152952) do
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   add_foreign_key "games", "reviews"
-  add_foreign_key "reviews", "games"
   add_foreign_key "reviews", "users"
 end
